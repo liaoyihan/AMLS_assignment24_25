20e479c53f29a6812768a5383ab7d5e61d873707
liaoyihan <106555445+liaoyihan@users.noreply.github.com>
2025-01-12 21:10:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 main function for two tasks, use to call main A and main B
 """
-import os
-os.system('python ./A/main_A.py')
-os.system('python ./B/main_B.py')
+import os  # Import the os module to interact with the operating system
+os.system('python ./A/main_A.py')  # Run the script for Task A
+os.system('python ./B/main_B.py')  # Run the script for Task B
